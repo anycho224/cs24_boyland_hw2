35c68d1e749e25373021af94e2206857b8a431e1
Anyi Chong <anyichong@csil.cs.ucsb.edu>
2025-10-17 01:15:44 -0700

    Added working undoredo implementation from lab to HW2 repo

--- undoredo/code/undoredo.cpp
+++ undoredo/code/undoredo.cpp
@@ -27,31 +27,35 @@ std::vector<std::string> get_args(const std::string& command) {
         args.push_back(current_arg);
     }
     return args;
-
-    struct Node{
-        int data;
-        int weight;
-        int index;
-        Node* next;
-        Node(int val) : data(val),weight(0),index(0),next(nullptr){}
-    };
 }
+struct Node{
+    std::string data;
+    int weight;
+    Node* next;
+};
 class Stack{
     private:
-        Node* top_stack;
-        Node* bot_stack;
+        Node* top;
+        Node* bottom;
         int total_weight;
+        int max_weight;
     public:
-        void print();
-        void push();
-        int pop();
-        void push(Node *n){
-            if(!bottom){
-                bottom = n;
-            }
+        void init(){
+            top = nullptr;
+            total_weight = 0;
+            max_weight=0;
+        }
+        void push(const std::string&data, int weight){
+            Node* n = new Node;
+            n->data=data;
+            n->weight=weight;
             n->next=top;
             top=n;
-            totalWeight+=n->weight;
+            total_weight+=n->weight;
+
+            while (total_weight>max_weight){
+                removeBottom();
+            }
         }
         Node* pop(){
             if(!top){
@@ -59,30 +63,48 @@ class Stack{
             }
             Node *n = top;
             top = top->next;
-            totalWeight-=n->weight;
-            delete top;
-            return top;
+            total_weight-=n->weight;
+            return n;
          }
-        void print() const{
-            Node *n = head;
-            while (n){
-                cout<< n->data;
-                if(n->next){
-                    std::cout << " ";
-                n = n->next;
-                }
-            }
-        }
         bool isEmpty(){
             top == nullptr;
             return top;
         }
+        void clear(){
+            while (top){
+                Node* n = top;
+                top=top->next;
+                delete n;
+
+            }
+            total_weight=0;
+        }
+    void removeBottom(){
+        Node* prev= nullptr;
+        Node* curr = top;
+        //traveres until it reaches the end;
+        while (curr->next!=nullptr){
+            prev=curr;
+            curr=curr->next;
+        }
+        total_weight-=curr->weight;
+        delete curr;
+        prev->next=nullptr;
+
+    }
 
 };
 
 int main() {
     int max_weight=0;
     std::string initial_string = "";
+    int weight =0;
+
+    Stack undoStack;
+    Stack redoStack;
+
+    undoStack.init();
+    redoStack.init();
 
     std::string line;
     int weight;
@@ -96,50 +118,62 @@ int main() {
 
         // Example of how to handle a command
         if (command == "PRINT") {
-            std::cout<< initial_tring << std::endl;
+            std::cout<< initial_string << std::endl;
         }
         else if (command == "CREATE"){
             max_weight = std::stoi(args[1]);
             initial_string = args[2];
-            std::cout << "CREATE" + max_weight + "This is the initial text";
+            std::cout << "CREATE" << max_weight << " \"" << initial_string << "\"" << std::endl;
         }
         else if (command == "APPEND"){
             std::string string_to_append= args[1];
+            undoStack.push(initial_string,string_to_append.size());
+            redoStack.clear();
             initial_string += string_to_append;
-            std::cout << "APPEND" + initial_string;
+            std::cout << "APPEND" << initial_string << std::endl;
         }
         //args[0]="REPLACE" args[1]="t" args[2]= "T"
         else if (command == "REPLACE"){
             char find_char= args[0][1];
             char replace_char = args[0][2];
             int count=0;
+            undoStack.push(initial_string,0);
             for(char& c : initial_string){
                 if(c==find_char){
                     c=replace_char;
                     count++;
                 }
             }
-            std::cout<<"REPLACE" + find_char + " " + replace_char;
+            std::cout<<"REPLACE" << find_char << " " << replace_char << std::endl;
         }
         else if(command == "DELETE"){
             int index= stoi(args[1]);
             int count=0;
-            std::string new_string = "";
-            for (int i=0; i<index;i++){
-                new_string+=initial_string[i];
-            }
-            weight=initial_string.size()-index;        
-            initial_string=new_string;
+            undoStack.push(initial_string,initial_string.size()-index);
+            redoStack.clear();
+            initial_string= initial_string.substr(0,index);
             std::cout<< "Delete" + weight << std::endl;
         }
         else if(command == "UNDO"){
-            if(undoStack.isempty()){
+            if(undoStack.isEmpty()){
                 std::cout << "Error: Nothing to undo" << std::endl;
+            }
+            else{
+                Node* last_undo = undoStack.pop();
+                redoStack.push(initial_string,last_undo->weight);
+                initial_string=last_undo->data;
+                delete last_undo;
             }
         }
         else if(command == "REDO"){
-            if(undoStack.empty()){
+            if(undoStack.isEmpty()){
                 std::cout<< "Error: Nothing to Redo" << std::endl;
+            }
+            else{
+                Node* last_redo=redoStack.pop();
+                undoStack.push(initial_string,last_redo->weight);
+                initial_string=last_redo->data;
+                delete last_redo;
             }
         }
         // TODO: Handle other commands: CREATE, APPEND, REPLACE, DELETE, UNDO, REDO
