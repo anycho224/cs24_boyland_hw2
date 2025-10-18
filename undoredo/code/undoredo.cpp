e6823f319aacd7dfaa40a8e9b5bafe6b7b63b994
Anyi Chong <anyichong@csil.cs.ucsb.edu>
2025-10-18 12:03:16 -0700

    Actual new changes saved

--- undoredo/code/undoredo.cpp
+++ undoredo/code/undoredo.cpp
@@ -145,6 +145,7 @@ int main() {
             undoStack.setMaxWeight(max_weight);
             redoStack.setMaxWeight(max_weight);
             std::cerr << "CREATE " << max_weight << " \"" << initial_string << "\"" << std::endl;
+            std::cerr <<initial_string << std::endl;
         }
         else if (command == "APPEND"){
             std::string string_to_append= "";
@@ -160,6 +161,7 @@ int main() {
             redoStack.clear();
             initial_string += string_to_append;
             std::cerr << "APPEND " << initial_string << std::endl;
+            std::cerr <<initial_string << std::endl;
         }
         else if (command == "REPLACE"){
             char find_char= args[1][0];
@@ -174,6 +176,7 @@ int main() {
                 }
             }
             std::cerr<<"REPLACE " << find_char << " " << replace_char << std::endl;
+            std::cerr <<initial_string << std::endl;
         }
         else if(command == "DELETE"){
             int index= std::stoi(args[1]);
@@ -181,6 +184,7 @@ int main() {
             redoStack.clear();
             initial_string= initial_string.substr(0,index);
             std::cerr<< "DELETE " << initial_string << std::endl;
+            std::cerr <<initial_string << std::endl;
         }
         else if(command == "UNDO"){
             if(undoStack.isEmpty()){
@@ -192,6 +196,7 @@ int main() {
                 initial_string=last_undo->data;
                 delete last_undo;
                 std::cerr << "UNDO " << initial_string << std::endl;
+                std::cerr <<initial_string << std::endl;
             }
         }
         else if(command == "REDO"){
@@ -204,12 +209,12 @@ int main() {
                 initial_string=last_redo->data;
                 delete last_redo;
                 std::cerr<< "REDO " << initial_string << std::endl;
+                std::cerr <<initial_string << std::endl;
             }
         }
         else if (command == "PRINT") {
             std::cerr<< initial_string << std::endl;
         }
     }
-    std::cerr <<initial_string << std::endl;
     return 0;
 }
