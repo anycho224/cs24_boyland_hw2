dac6213ce80bfbd211e0a73abeaa3b1eb326c09f
Anyi Chong <anyichong@csil.cs.ucsb.edu>
2025-10-17 21:59:20 -0700

    Actual new changes saved

--- undoredo/code/undoredo.cpp
+++ undoredo/code/undoredo.cpp
@@ -141,14 +141,15 @@ int main() {
         }
         else if (command == "APPEND"){
             std::string string_to_append= "";
-            for(int i=1; i<args.size();i++){
+            int count=args.size();
+            for(int i=1; i<count;i++){
                 if(i>1){
                     string_to_append+= " ";
                 }
                 string_to_append+=args[i];
             }
             int weight = string_to_append.length();
-            undoStack.push(initial_string,string_to_append.size());
+            undoStack.push(initial_string,weight);
             redoStack.clear();
             initial_string += string_to_append;
             std::cout << "APPEND " << initial_string << std::endl;
