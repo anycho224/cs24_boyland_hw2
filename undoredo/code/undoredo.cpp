29869df5201703ad17a5ec60a66e75be5f435e1c
Anyi Chong <anyichong@csil.cs.ucsb.edu>
2025-10-18 12:07:56 -0700

    Actual new changes saved

--- undoredo/code/undoredo.cpp
+++ undoredo/code/undoredo.cpp
@@ -144,8 +144,8 @@ int main() {
             initial_string=new_string;
             undoStack.setMaxWeight(max_weight);
             redoStack.setMaxWeight(max_weight);
-            std::cerr << "CREATE " << max_weight << " \"" << initial_string << "\"" << std::endl;
-            std::cerr <<initial_string << std::endl;
+            std::cout << "CREATE " << max_weight << " \"" << initial_string << "\"" << std::endl;
+            std::cout <<initial_string << std::endl;
         }
         else if (command == "APPEND"){
             std::string string_to_append= "";
@@ -160,8 +160,8 @@ int main() {
             undoStack.push(initial_string, weight);
             redoStack.clear();
             initial_string += string_to_append;
-            std::cerr << "APPEND " << initial_string << std::endl;
-            std::cerr <<initial_string << std::endl;
+            std::cout << "APPEND " << initial_string << std::endl;
+            std::cout <<initial_string << std::endl;
         }
         else if (command == "REPLACE"){
             char find_char= args[1][0];
@@ -175,45 +175,45 @@ int main() {
                     count++;
                 }
             }
-            std::cerr<<"REPLACE " << find_char << " " << replace_char << std::endl;
-            std::cerr <<initial_string << std::endl;
+            std::cout<<"REPLACE " << find_char << " " << replace_char << std::endl;
+            std::cout <<initial_string << std::endl;
         }
         else if(command == "DELETE"){
             int index= std::stoi(args[1]);
             undoStack.push(initial_string,initial_string.size()-index);
             redoStack.clear();
             initial_string= initial_string.substr(0,index);
-            std::cerr<< "DELETE " << initial_string << std::endl;
-            std::cerr <<initial_string << std::endl;
+            std::cout << "DELETE " << initial_string << std::endl;
+            std::cout <<initial_string << std::endl;
         }
         else if(command == "UNDO"){
             if(undoStack.isEmpty()){
-                std::cerr << "Error: Nothing to undo." << std::endl;
+                std::cout << "Error: Nothing to undo." << std::endl;
             }
             else{
                 Node* last_undo = undoStack.pop();
                 redoStack.push(initial_string,last_undo->weight);
                 initial_string=last_undo->data;
                 delete last_undo;
-                std::cerr << "UNDO " << initial_string << std::endl;
-                std::cerr <<initial_string << std::endl;
+                std::cout << "UNDO " << initial_string << std::endl;
+                std::cout <<initial_string << std::endl;
             }
         }
         else if(command == "REDO"){
             if(redoStack.isEmpty()){
-                std::cerr<< "Error: Nothing to redo." << std::endl;
+                std::cout << "Error: Nothing to redo." << std::endl;
             }
             else{
                 Node* last_redo=redoStack.pop();
                 undoStack.push(initial_string,last_redo->weight);
                 initial_string=last_redo->data;
                 delete last_redo;
-                std::cerr<< "REDO " << initial_string << std::endl;
-                std::cerr <<initial_string << std::endl;
+                std::cout << "REDO " << initial_string << std::endl;
+                std::cout  <<initial_string << std::endl;
             }
         }
         else if (command == "PRINT") {
-            std::cerr<< initial_string << std::endl;
+            std::cout << initial_string << std::endl;
         }
     }
     return 0;
